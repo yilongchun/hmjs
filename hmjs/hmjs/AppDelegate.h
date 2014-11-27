1264deda6009a41afebc5953a904368ec2baf30f
yilongchun <5115413@qq.com>
2014-11-27 14:59:41 +0000

    创建项目

---
+++ hmjs/hmjs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hmjs
+//
+//  Created by yons on 14-11-27.
+//  Copyright (c) 2014年 yons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hmjs/hmjs/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hmjs
+//
+//  Created by yons on 14-11-27.
+//  Copyright (c) 2014年 yons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
